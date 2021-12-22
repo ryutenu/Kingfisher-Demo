635b11d39a4eaefbd1e3234698d6aea012e93721
Alan Liu <ryutenu.tokyo@gmail.com>
2021-12-22 23:13:51 +0900

    [Add] Kingfisher

--- Kingfisher-Demo/ViewController.swift
+++ Kingfisher-Demo/ViewController.swift
@@ -6,14 +6,12 @@
 //
 
 import UIKit
+import Kingfisher
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
-
-
 }
-
---
+++ Pods/Target Support Files/Pods-Kingfisher-Demo/Pods-Kingfisher-Demo-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Kingfisher_Demo : NSObject
+@end
+@implementation PodsDummy_Pods_Kingfisher_Demo
+@end
